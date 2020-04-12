103f87dcc7b832218716b95d2fc7a7c6ee024161
yujian <yujian1018@gmail.com>
2020-04-12 23:18:56 +0800

    支持自定义文件名称

--- src/aof/aof_file.erl
+++ src/aof/aof_file.erl
@@ -17,8 +17,16 @@
 load_file(Config) ->
     case application:get_env(cache, dir) of
         {ok, Path} ->
-            TabStr = atom_to_list(Config#cache_mate.name),
-            {ok, [Context]} = file:consult(filename:join([Path]) ++ "/" ++ TabStr ++ ".data"),
+            FileName =
+                if
+                    is_atom(Config#cache_mate.name) ->
+                        filename:join([Path]) ++ "/" ++ atom_to_list(Config#cache_mate.name) ++ ".data";
+                    is_list(Config#cache_mate.name) ->
+                        Config#cache_mate.name;
+                    true ->
+                        ?ERROR("Config Err filename is  null")
+                end,
+            {ok, [Context]} = file:consult(FileName),
             Md5Context = erlang:md5_init(),
             {NewMd5Context, FieldRecords} = aof_mysql:cache_data(Config, Context, Md5Context, Context),
             cache_behaviour:cache_data(Config, erlang:md5_final(NewMd5Context), FieldRecords, Context);
